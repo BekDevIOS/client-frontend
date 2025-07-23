3d3e640c59fd3df39d976d9a1c0d2af5b8edd340
BekDevIOS <umekhmonboev@gmail.com>
2025-07-23 17:44:04 +0900

    fix: organize header and footer components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,45 +1,35 @@
 import React from 'react';
 import '../css/app.css';
-import { Link, Route, Switch } from 'react-router-dom';
+import { Link, Route, Switch, useLocation } from 'react-router-dom';
 import { HomePage } from './screens/homePage';
 import { ProductsPage } from './screens/productsPage';
 import { OrdersPage } from './screens/ordersPage';
 import { UserPage } from './screens/userPage';
+import { HomeNavbar } from './components/headers/HomeNavbar';
+import { OtherNavbar } from './components/headers/OtherNavbar';
+import { Footer } from './components/footer';
 
 export default function App() {
+  const location = useLocation();
+  console.log('location',location);
   return (
-    <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">HomePage</Link>
-            </li>
-            <li>
-              <Link to="/products">ProductsPage</Link>
-            </li>
-            <li>
-              <Link to="/orders">OrdersPage</Link>
-            </li>
-            <li>
-              <Link to="/member-page">UserPage</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Switch>
-          <Route path="/products">
-            <ProductsPage />
-          </Route>
-          <Route path="/orders">
-            <OrdersPage />
-          </Route>
-          <Route path="/member-page">
-            <UserPage />
-          </Route>
-          <Route path="/">
-            <HomePage />
-          </Route>
-        </Switch>
-      </div>
+    <>
+      {location.pathname === '/' ? <HomeNavbar /> : <OtherNavbar />}
+      <Switch>
+        <Route path="/products">
+          <ProductsPage />
+        </Route>
+        <Route path="/orders">
+           <OrdersPage />
+        </Route>
+        <Route path="/member-page">
+          <UserPage />
+        </Route>
+        <Route path="/">
+          <HomePage />
+        </Route>
+      </Switch>
+      <Footer />
+    </>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
-import App from './app/App';
+import  App  from './app/App';
 import reportWebVitals from './reportWebVitals';
 import  CssBaseline  from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
