6a4e969a6b47468c372f421100aa35d42ae26c9a
BekDevIOS <umekhmonboev@gmail.com>
2025-07-26 19:45:30 +0900

    feat: integration footer and basket source

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Link, Route, Switch, useLocation } from 'react-router-dom';
-import { HomePage } from './screens/homePage';
-import { ProductsPage } from './screens/productsPage';
-import { OrdersPage } from './screens/ordersPage';
-import { UserPage } from './screens/userPage';
-import { HomeNavbar } from './components/headers/HomeNavbar';
-import { OtherNavbar } from './components/headers/OtherNavbar';
-import { Footer } from './components/footer';
+import  HomePage  from './screens/homePage';
+import  ProductsPage  from './screens/productsPage';
+import  OrdersPage  from './screens/ordersPage';
+import  UserPage  from './screens/userPage';
+import  HomeNavbar  from './components/headers/HomeNavbar';
+import  OtherNavbar  from './components/headers/OtherNavbar';
+import  Footer  from './components/footer';
+import HelpPage from './screens/helpPage';
 import '../css/app.css';
 import '../css/navbar.css';
+import '../css/footer.css';
 
 export default function App() {
   const location = useLocation();
@@ -25,6 +27,9 @@ export default function App() {
         </Route>
         <Route path="/member-page">
           <UserPage />
+        </Route>
+        <Route path="/help">
+          <HelpPage/>
         </Route>
         <Route path="/">
           <HomePage />
