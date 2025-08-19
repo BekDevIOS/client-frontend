0416d027168f1bbcbc8962246cb6e4e843cad439
BekDevIOS <umekhmonboev@gmail.com>
2025-08-19 11:48:50 +0900

    feat: create homePage Redux contifuration

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Statistics from "./Statistics";
 import PopularDishes from "./PopularDishes";
 import NewDishes from "./NewDishes";
@@ -8,12 +8,18 @@ import Events from "./Events";
 import "../../../css/home.css";
 
 export default function HomePage() {
-  return <div className="homepage">
-    <Statistics/>
-    <PopularDishes/>
-    <NewDishes/>
-    <Advertisement/>
-    <ActiveUsers/>
-    <Events/>
-  </div>;
-};
+  //selector: store = > date
+
+  useEffect(() => {}, []);
+
+  return (
+    <div className="homepage">
+      <Statistics />
+      <PopularDishes />
+      <NewDishes />
+      <Advertisement />
+      <ActiveUsers />
+      <Events />
+    </div>
+  );
+}
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,12 +1,18 @@
 // src/app/store.ts
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from './counterSlice'; // adjust if path differs
+import { Action, configureStore, ThunkAction } from "@reduxjs/toolkit";
+import HomePageReducer from "./screens/homePage/slice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    homePage: HomePageReducer,
   },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
