fdb70341c09f997e49a70af60e86beaa0a30615c
BekDevIOS <umekhmonboev@gmail.com>
2025-08-20 21:10:43 +0900

    fix: retreive top users, newDishes and modify ActiveUsers, FreshMenu component

--- src/app/screens/homePage/NewDishes.tsx
+++ src/app/screens/homePage/NewDishes.tsx
@@ -2,66 +2,80 @@ import React from "react";
 import AspectRatio from "@mui/joy/AspectRatio";
 import { Box, Container, Stack } from "@mui/material";
 import { CssVarsProvider } from "@mui/joy/styles";
-import Card from '@mui/joy/Card';
-import Typography from '@mui/joy/Typography';
+import Card from "@mui/joy/Card";
+import Typography from "@mui/joy/Typography";
 import CardOverflow from "@mui/joy/CardOverflow";
 import VisibilityIcon from "@mui/icons-material/Visibility";
 import Divider from "../../components/divider";
+import { useSelector } from "react-redux";
+import { createSelector } from "reselect";
+import { retrieveNewDishes } from "./selector";
+import { Product } from "../../../lib/types/product";
+import { ProductCollection } from "../../../lib/enums/product.enums";
+import { serverApi } from "../../../lib/config";
 
-const newDishes = [
-  {productName: "Cutlet", imagePath: "/img/cutlet.webp"},
-  {productName: "Kebab", imagePath: "/img/kebab.webp"},
-  {productName: "Lavash", imagePath: "/img/lavash.webp"},
-  {productName: "Kebab", imagePath: "/img/kebab-fresh.webp"},
-];
-export default function NewDishes(){
+/** REDUX SLICE & SELECTOR */
+
+const newDishesRetriever = createSelector(retrieveNewDishes, (newDishes) => ({
+  newDishes,
+}));
+
+export default function NewDishes() {
+  const { newDishes } = useSelector(newDishesRetriever);
   return (
-  <div className="new-products-frame">
-    <Container>
-      <Stack className="main">
-        <Box className="category-title">Fresh Menu</Box>
-        <Stack className="cards-frame">
-          <CssVarsProvider>
-            {newDishes.length !==0 ? (
-              newDishes.map((val, index) => {
-                return (
-                  <Card key={index} variant="outlined" className="card">
-                    <CardOverflow>
-                      <div className="product-sale">Normal size</div>
-                      <AspectRatio ratio={"1"}>
-                        <img src={val.imagePath} alt="" />
-                      </AspectRatio>
-                    </CardOverflow>
+    <div className="new-products-frame">
+      <Container>
+        <Stack className="main">
+          <Box className="category-title">Fresh Menu</Box>
+          <Stack className="cards-frame">
+            <CssVarsProvider>
+              {newDishes.length !== 0 ? (
+                newDishes.map((product: Product) => {
+                  const imagePath = `${serverApi}/${product.productImages[0]}`;
+                  const sizeVolume =
+                    product.productCollection === ProductCollection.DRINK
+                      ? product.productVolume + "L"
+                      : product.productSize + "size";
+                  return (
+                    <Card key={product._id} variant="outlined" className="card">
+                      <CardOverflow>
+                        <div className="product-sale">Normal size</div>
+                        <AspectRatio ratio={"1"}>
+                          <img src={imagePath} alt="" />
+                        </AspectRatio>
+                      </CardOverflow>
 
-                    <CardOverflow variant="soft" className="product-detail">
-                      <Stack className="info">
-                        <Stack flexDirection={"row"}>
-                          <Typography className="title">
-                            {val.productName}
-                          </Typography>
-                          <Divider width="2" height="24" bg="#d9d9d9"/>
-                          <Typography className="price">$12</Typography>
+                      <CardOverflow variant="soft" className="product-detail">
+                        <Stack className="info">
+                          <Stack flexDirection={"row"}>
+                            <Typography className="title">
+                              {product.productName}
+                            </Typography>
+                            <Divider width="2" height="24" bg="#d9d9d9" />
+                            <Typography className="price">
+                              {product.productPrice}
+                            </Typography>
+                          </Stack>
+                          <Stack>
+                            <Typography className="views">
+                              {product.productViews}
+                              <VisibilityIcon
+                                sx={{ fontSize: 20, marginLeft: "5px" }}
+                              />
+                            </Typography>
+                          </Stack>
                         </Stack>
-                        <Stack>
-                          <Typography className="views">
-                            20
-                            <VisibilityIcon
-                              sx={{fontSize: 20, marginLeft: "5px"}}
-                            />
-                          </Typography>
-                        </Stack>
-                      </Stack>
-                    </CardOverflow>
-                  </Card>
-                );
-              })
-            ) : (
-              <Box className="no-data">New products are not available!</Box>
-            )}
-          </CssVarsProvider>
+                      </CardOverflow>
+                    </Card>
+                  );
+                })
+              ) : (
+                <Box className="no-data">New products are not available!</Box>
+              )}
+            </CssVarsProvider>
+          </Stack>
         </Stack>
-      </Stack>
-    </Container>
-  </div>
+      </Container>
+    </div>
   );
 }
--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -5,30 +5,64 @@ import NewDishes from "./NewDishes";
 import Advertisement from "./Advertisement";
 import ActiveUsers from "./ActiveUsers";
 import Events from "./Events";
-import "../../../css/home.css";
-
 import { useDispatch, useSelector } from "react-redux";
 import { Dispatch } from "@reduxjs/toolkit";
-import { createSelector } from "reselect";
-import { setPopularDishes } from "./slice";
-import { retrievePopularDishes } from "./selector";
+import { setNewDishes, setPopularDishes, setTopUsers } from "./slice";
 import { Product } from "../../../lib/types/product";
+import ProductService from "../../services/ProductService";
+import { ProductCollection } from "../../../lib/enums/product.enums";
+import MemberService from "../../services/MemberService";
+import { Member } from "../../../lib/types/member";
+import "../../../css/home.css";
 
 /** REDUX SLICE & SELECTOR */
 const actionDispatch = (dispatch: Dispatch) => ({
   setPopularDishes: (data: Product[]) => dispatch(setPopularDishes(data)),
+  setNewDishes: (data: Product[]) => dispatch(setNewDishes(data)),
+  setTopUsers: (data: Member[]) => dispatch(setTopUsers(data)),
 });
-const popularDishesRetriever = createSelector(
-  retrievePopularDishes,
-  (popularDishes) => ({ popularDishes })
-);
 
 export default function HomePage() {
-  const { setPopularDishes } = actionDispatch(useDispatch());
-  const { popularDishes } = useSelector(popularDishesRetriever);
+  const { setPopularDishes, setNewDishes, setTopUsers } = actionDispatch(
+    useDispatch()
+  );
 
+  useEffect(() => {
+    const product = new ProductService();
+    product
+      .getProducts({
+        page: 1,
+        limit: 4,
+        order: "productViews",
+        // productCollection: ProductCollection.DISH,
+      })
+      .then((data) => {
+        setPopularDishes(data);
+      })
+      .catch((err) => console.log(err));
 
-  useEffect(() => {}, []);
+    product
+      .getProducts({
+        page: 1,
+        limit: 4,
+        order: "createdAt",
+        // productCollection: ProductCollection.DISH,
+      })
+      .then((data) => {
+        setNewDishes(data);
+      })
+      .catch((err) => console.log(err));
+
+    const member = new MemberService();
+    member
+      .getTopUsers()
+      .then((data) => {
+        setTopUsers(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
   return (
     <div className="homepage">
